7ff7439d00fca7cb931b877b05c07b8a1cc36ce7
Christopher Biscardi <biscarch@sketcht.com>
2018-12-26 15:31:55 -0800

    icon

--- ui/www/gatsby-config.js
+++ ui/www/gatsby-config.js
@@ -27,13 +27,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-default-mdx-basic",
-        short_name: "starter",
+        name: "mntnr-systems",
+        short_name: "mntnrs",
         start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
+        background_color: "#f1b8e8",
+        theme_color: "#f1b8e8",
         display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
+        icon: "src/images/icon.png" // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
